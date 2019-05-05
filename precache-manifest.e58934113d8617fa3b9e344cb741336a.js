e0d48a82c9c4970c0dbbb56038fdfcafbc91fa06
Alejandro Manza <alejandromanza@gmail.com>
2019-05-05 16:56:48 +0200

    Initial dist subtree commit

---
+++ precache-manifest.e58934113d8617fa3b9e344cb741336a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/untitled/robots.txt"
+  },
+  {
+    "revision": "4a7d44f95fb5ebcdace1",
+    "url": "/untitled/js/chunk-vendors.ab4177f8.js"
+  },
+  {
+    "revision": "7139d6f3f3091adc0b4c",
+    "url": "/untitled/js/app.36ff6fa4.js"
+  },
+  {
+    "revision": "2b68f8e3797347696f5f",
+    "url": "/untitled/js/about.78516d6b.js"
+  },
+  {
+    "revision": "f39b961a0d375443078cbdb8b4cf3151",
+    "url": "/untitled/index.html"
+  },
+  {
+    "revision": "4a7d44f95fb5ebcdace1",
+    "url": "/untitled/css/chunk-vendors.9382de7d.css"
+  },
+  {
+    "revision": "7139d6f3f3091adc0b4c",
+    "url": "/untitled/css/app.1dd5c102.css"
+  }
+];
